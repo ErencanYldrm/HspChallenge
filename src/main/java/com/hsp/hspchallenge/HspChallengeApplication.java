123ff4a41d874c4f9f20d0e53b6adb461e9a5840
ErencanYldrm <96621237+ErencanYldrm@users.noreply.github.com>
2023-07-06 13:58:40 +0300

    Project created and docker-compose.yml file created

---
+++ src/main/java/com/hsp/hspchallenge/HspChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.hsp.hspchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsp/hspchallenge/HspChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsp.hspchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HspChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
