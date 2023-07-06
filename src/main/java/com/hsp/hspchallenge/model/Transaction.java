4e3a8c72c3a38fc0684fa6aa82cdf76b502993b5
ErencanYldrm <96621237+ErencanYldrm@users.noreply.github.com>
2023-07-06 19:10:23 +0300

    Transaction model created.

---
+++ src/main/java/com/hsp/hspchallenge/model/Transaction.java
@@ -0,0 +1,39 @@
+package com.hsp.hspchallenge.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transactions")
+public class Transaction {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant date = Instant.now();
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+}
